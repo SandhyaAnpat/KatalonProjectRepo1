55c5a5c3a7c79bbd029298bd7186d38d864342f7
SandhyaAnpat <sandhya.anpat0489@gmail.com>
2023-11-03 19:01:02 +0530

    Added BDD testcases

---
+++ Test Suites/PracticeJenkins1.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>PracticeJenkins1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>ac510ae5-dfc5-443b-8af5-0285f1e55f8a</testSuiteGuid>
+</TestSuiteEntity>
